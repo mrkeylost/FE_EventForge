43b582d0f7da4a3c6d93e0fb2a9e60db962d48de
mrkeylost <nikosam08@gmail.com>
2026-05-07 12:47:37 +0700

    style: adjust heroUI to v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import PageHead from "@/components/commons/PageHead/PageHead";
-import { Button } from "@heroui/react";
 
 export default function Home() {
   return (
     <main className="mx-auto flex min-h-screen w-full max-w-3xl flex-col items-center justify-center bg-white dark:bg-black">
       <PageHead />
-      <Button variant="primary">Primary</Button>
     </main>
   );
 }
